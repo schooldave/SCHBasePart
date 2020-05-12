be00294ea5ad5ea45cc5d72afc789c3d992b1056
David <schooldave@sina.com>
2020-05-12 23:25:25 +0800

    setUp

---
+++ SCHBasePart/SCHBasePart/SCHBasePartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SCHBasePartViewController.swift
+//  SCHBasePart
+//
+//  Created by schooldave on 5/12/20.
+//  Copyright © 2020 schooldave. All rights reserved.
+//
+
+import UIKit
+
+class SCHBasePartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
